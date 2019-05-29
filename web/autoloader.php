e1b6163bf7083efa9d7a8d3d4e5b18de5819de15
CaupoHelvik <helvikcaupo@gmail.com>
2019-05-29 13:23:00 +0300

    - autoloaderi faili järjestuse fix

--- web/autoloader.php
+++ web/autoloader.php
@@ -2,8 +2,8 @@
 $files = [
 	__DIR__ . '/../components/Router.php',
 	__DIR__ . '/../components/QueryBuilder.php',
-	__DIR__ . '/../models/University.php',
 	__DIR__ . '/../models/BaseModel.php',
+	__DIR__ . '/../models/University.php',
 	__DIR__ . '/../models/Test.php',
 	__DIR__ . '/../controllers/BaseController.php',
 	__DIR__ . '/../controllers/SiteController.php'
