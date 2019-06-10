8e693ea886240e8ade8c71342228fb1e2bbb4059
plaupa <priit.laupa@gmail.com>
2019-06-10 16:03:05 +0300

    - Alustatud otsimise ja filtreerimise funktsioonidega main.js'is - university classidele pandud ümber div index.php's

--- views/site/index.php
+++ views/site/index.php
@@ -32,40 +32,42 @@
 
 	<div id="search-results">
 		<h2>Ülikoolid</h2>
-		<div class="university">
-			<h3>Ülikool 1</h3>
-			<div class="university-description">
-				<div class="university-icon"><img src="https://pbs.twimg.com/profile_images/679594326691741696/of9OpXVv.png"></div>
-				<div class="description-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
-				<div class="percent-with-link">
-					<div>80%</div>
-					<div><a href="#">wwwcom</a></div>
+		<div class="universities">
+			<div class="university">
+				<h3>Ülikool 1</h3>
+				<div class="university-description">
+					<div class="university-icon"><img src="https://pbs.twimg.com/profile_images/679594326691741696/of9OpXVv.png"></div>
+					<div class="description-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
+					<div class="percent-with-link">
+						<div>80%</div>
+						<div><a href="#">wwwcom</a></div>
+					</div>
+					<div class="university-map"><img src="https://custom-map-source.appspot.com/galileo-google-maps.png"></div>
 				</div>
-				<div class="university-map"><img src="https://custom-map-source.appspot.com/galileo-google-maps.png"></div>
 			</div>
-		</div>
-		<div class="university">
-			<h3>Ülikool 1</h3>
-			<div class="university-description">
-				<div class="university-icon"><img src="https://pbs.twimg.com/profile_images/679594326691741696/of9OpXVv.png"></div>
-				<div class="description-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
-				<div class="percent-with-link">
-					<div>80%</div>
-					<div><a href="#">wwwcom</a></div>
+			<div class="university">
+				<h3>Ülikool 1</h3>
+				<div class="university-description">
+					<div class="university-icon"><img src="https://pbs.twimg.com/profile_images/679594326691741696/of9OpXVv.png"></div>
+					<div class="description-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
+					<div class="percent-with-link">
+						<div>80%</div>
+						<div><a href="#">wwwcom</a></div>
+					</div>
+					<div class="university-map"><img src="https://custom-map-source.appspot.com/galileo-google-maps.png"></div>
 				</div>
-				<div class="university-map"><img src="https://custom-map-source.appspot.com/galileo-google-maps.png"></div>
 			</div>
-		</div>
-		<div class="university">
-			<h3>Ülikool 1</h3>
-			<div class="university-description">
-				<div class="university-icon"><img src="https://pbs.twimg.com/profile_images/679594326691741696/of9OpXVv.png"></div>
-				<div class="description-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
-				<div class="percent-with-link">
-					<div>80%</div>
-					<div><a href="#">wwwcom</a></div>
+			<div class="university">
+				<h3>Ülikool 1</h3>
+				<div class="university-description">
+					<div class="university-icon"><img src="https://pbs.twimg.com/profile_images/679594326691741696/of9OpXVv.png"></div>
+					<div class="description-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
+					<div class="percent-with-link">
+						<div>80%</div>
+						<div><a href="#">wwwcom</a></div>
+					</div>
+					<div class="university-map"><img src="https://custom-map-source.appspot.com/galileo-google-maps.png"></div>
 				</div>
-				<div class="university-map"><img src="https://custom-map-source.appspot.com/galileo-google-maps.png"></div>
 			</div>
 		</div>
 	</div>
