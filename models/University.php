7ffbca8bca53fb94ba221e4dc2aab926ebebe80d
plaupa <priit.laupa@gmail.com>
2019-06-12 22:51:18 +0300

    - Lisatud University

--- models/University.php
+++ models/University.php
@@ -2,6 +2,7 @@
 
 namespace app\models;
 
+use app\components\QueryBuilder;
 use app\components\ActiveRecord;
 
 class University extends ActiveRecord {
@@ -10,7 +11,7 @@ class University extends ActiveRecord {
 		return "university";
 	}
 	
-	public function rules(){
+	public function rules() {
 		return[
 			[['name', 'country', 'created_at'], ["string"]],
 			[['contact_email'], ['email']],
@@ -24,10 +25,26 @@ class University extends ActiveRecord {
         parent::beforeSave();
 	}
 	
-	public function resetSearchIndex(){
-		QueryBuilder::delete(self::tableName(), ["=", ]);
-		foreach ($departments as $key => $value) {
-			# code...
+	public function resetSearchIndex() {
+		$departments = QueryBuilder::select(self::tableName())->addWhere("=", "university_id", $this->id)->queryAll();
+		foreach ($departments as $department) {
+			$str = $department->name;
+			$words = explode(" ", $str);
+			foreach ($words as $word) {
+				$keyword = new SearchIndex();
+				$keyword->$university_id = $this->id;
+				$keyword->keyword = $word;
+			}
+		}
+		$specialities = QueryBuilder::select(self::tableName())->addWhere("=", "department_id", $this->department_id)->queryAll();
+		foreach ($specialities as $speciality) {
+			$str = $speciality->name;
+			$words = explode(" ", $str);
+			foreach ($words as $word) {
+				$keyword = new SearchIndex();
+				$keyword->$university_id = $this->id;
+				$keyword->keyword = $word;
+			}
 		}
 	}
 }
